ab243ff923ceb729367447c93a528292dab175b5
ISAAM <isaam_99@hotmail.com>
2025-10-02 10:44:08 -0500

    archivo vist

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/saludo', function () {
+    return ('g');
+});
+
+Route::get('/bienvenido', function () {
+    return view('bienvenido');
+});
